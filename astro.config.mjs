5cdced20db15a7543968997d0e88091682039527
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-03 20:25:59 +0200

    Done mail not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  site: 'https://uqnc.github.io',
+  base: '/aperium',
 });
