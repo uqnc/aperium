8bddf278046a0d4d09ac35046c7359ed1dde166f
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-03 22:19:14 +0200

    NOT WORKING

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		screens: {
@@ -7,12 +8,15 @@ export default {
 			'xs': '350px',
 			'sm': '390px',
 			'md': '768px',
+			'lg': '1024px',
+			'xl': '1920px'
 		},
 		extend: {
 			fontFamily: {
 				satoshi: ['Satoshi', 'sans-serif'],
 				inter: ['Inter', 'sans-serif'],
 				roboto: ['Roboto', 'sans-serif'],
+				monnserrat: ['Montserrat', 'sans-serif']
 			},
 		},
 	},
