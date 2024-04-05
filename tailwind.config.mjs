37c10697bbf910ce3af3784a3ec1926e420aeaf1
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-05 17:05:42 +0200

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/preline/preline.js',
+	],
 	theme: {
-		screens: {
-			'xss': '280px',
-			'xs': '370px',
-			'sm': '390px',
-			'xm': '414px',
-			'xlm': '425px',
-			'md': '768px',
-			'lg': '1124px',
-			'xl': '1920px'
-		},
 		extend: {
 			fontFamily: {
 				satoshi: ['Satoshi', 'sans-serif'],
-				inter: ['Inter', 'sans-serif'],
-				roboto: ['Roboto', 'sans-serif'],
-				monnserrat: ['Montserrat', 'sans-serif']
+				inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin'),
+	],
+	darkMode: 'class',
 }
